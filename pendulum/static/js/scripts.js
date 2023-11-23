1c45caa2bac6f6d4e4acf1693d800f9bad887378
Timur <tmyaldzin13@gmail.com>
2023-11-23 23:21:39 +0300

    created the functionality for the buttons to work

--- pendulum/static/js/scripts.js
+++ pendulum/static/js/scripts.js
@@ -1,46 +1,60 @@
 function solveEquation(k, g, l, a, w, t) {
-    const dt = 0.01;
-    const numSteps = 1000;
-  
-    let f = 0.01;
-    let dfdt = 0;
-  
-    const angles = []; // список углов отклонения
-  
-    for (let i = 1; i < numSteps; i++) {
-      const d2fdt2 = -2 * k * dfdt - (g / l - a * w * w * Math.cos(w * i * dt) / l) * Math.sin(f);
-      dfdt += d2fdt2 * dt;
-      f += dfdt * dt;
-  
-      angles.push(f);
-    }
-  
-    return angles.reverse();
+  const dt = 0.01;
+  const numSteps = 1000;
+
+  let f = 0.01;
+  let dfdt = 0;
+
+  const angles = []; // список углов отклонения
+
+  for (let i = 1; i < numSteps; i++) {
+    const d2fdt2 = -2 * k * dfdt - (g / l - a * w * w * Math.cos(w * i * dt) / l) * Math.sin(f);
+    dfdt += d2fdt2 * dt;
+    f += dfdt * dt;
+
+    angles.push(f);
   }
-  
-  const k = 0.5; // коэффициент трения
-  const g = 9.8; // ускорение свободного падения
-  const l = 2; // длина маятника
-  const a = 0.2; // частота осцилляций
-  const w = 2; // частота
-  const t = 10; // время
-  
-  const anglesOfDeflection = solveEquation(k, g, l, a, w, t);
+
+  return angles.reverse();
+}
+
+function getDataAndPassToScript() {
+  var k = document.getElementById("k").value; // коэффициент трения
+  var g = 9.8; // ускорение свободного падения
+  var l = document.getElementById("l").value; // длина маятника
+  var a = document.getElementById("A").value; // частота осцилляций
+  var w = document.getElementById("w").value; // частота
+  var t = document.getElementById("t").value; // время
+
+  k = parseFloat(k)
+  l = parseFloat(l)
+  a = parseFloat(a)
+  w = parseFloat(w)
+  t = parseFloat(t)
+
+  var anglesOfDeflection = solveEquation(k, g, l, a, w, t);
   console.log(anglesOfDeflection);
-  
-  //строим график
-  const canvas = document.getElementById('myChart');
-  const ctx = canvas.getContext('2d');
-  
-  // Создаем новый график с использованием Chart.js
-  const myChart = new Chart(ctx, {
+
+  buildGraph(anglesOfDeflection)
+}
+
+var myChart
+
+function buildGraph(data) {
+
+  if (myChart) {
+    myChart.destroy();
+  }
+  var ctx = document.getElementById('myChart').getContext('2d');
+
+  myChart = new Chart(ctx, {
     type: 'line', // Указываем тип графика - линейный
     data: {
-      labels: anglesOfDeflection.map((_, index) => index + 1), // Создаем массив с метками для оси X
+      labels: data.map((_, index) => index + 1), // Создаем массив с метками для оси X
       datasets: [
         {
           label: 'Angle of deflection',
-          data: anglesOfDeflection,
+          data: data,
           backgroundColor: 'rgba(0, 123, 255, 0.5)',
           borderColor: 'rgba(0, 123, 255, 1)',
           borderWidth: 1
@@ -64,19 +78,32 @@ function solveEquation(k, g, l, a, w, t) {
     }
     }
   });
-  
-  // // Function to save the chart as an image
-  // function saveChartAsImage(chart) {
-  //   const image = chart.toBase64Image(); // Convert the chart to a base64-encoded image
-  
-  //   // Create a temporary link element
-  //   const link = document.createElement('a');
-  //   link.href = image;
-  //   link.download = 'chart.png'; // Set the desired file name
-  
-  //   // Programmatically click the link to trigger the download
-  //   link.click();
-  // }
-  
-  // // Example usage
-  // saveChartAsImage(myChart);
+}
+
+function downloadGraph() {
+  // Проверка наличия графика
+  if (!myChart) {
+      alert('Пожалуйста, постройте график перед загрузкой.');
+      return;
+  }
+
+  // Получение элемента canvas
+  var canvas = document.getElementById('myChart');
+
+  // Преобразование содержимого canvas в Blob
+  canvas.toBlob(function (blob) {
+      // Создание ссылки для скачивания
+      var link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'chart.png'; // Вы можете изменить имя файла и расширение при необходимости
+
+      // Добавление ссылки в документ
+      document.body.appendChild(link);
+
+      // Имитация клика по ссылке для начала загрузки
+      link.click();
+
+      // Удаление ссылки из документа
+      document.body.removeChild(link);
+  });
+}
