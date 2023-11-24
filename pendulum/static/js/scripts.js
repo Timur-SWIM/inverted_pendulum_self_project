1d230f5b420e7d3da0c0fe61896f074dc62c2c55
Timur <tmyaldzin13@gmail.com>
2023-11-24 03:24:48 +0300

    add animation

--- pendulum/static/js/scripts.js
+++ pendulum/static/js/scripts.js
@@ -20,25 +20,27 @@ function solveEquation(k, g, l, a, w, t) {
 
 function getDataAndPassToScript() {
   var k = document.getElementById("k").value; // коэффициент трения
-  var g = 9.8; // ускорение свободного падения
+  var g = document.getElementById("g").value; // ускорение свободного падения
   var l = document.getElementById("l").value; // длина маятника
   var a = document.getElementById("A").value; // частота осцилляций
   var w = document.getElementById("w").value; // частота
   var t = document.getElementById("t").value; // время
 
   // обновление значений
-  addToTable(k, g, l, a, w, t)
-
-  k = parseFloat(k)
-  l = parseFloat(l)
-  a = parseFloat(a)
-  w = parseFloat(w)
-  t = parseFloat(t)
+  addToTable(k, g, l, a, w, t);
 
   var anglesOfDeflection = solveEquation(k, g, l, a, w, t);
   console.log(anglesOfDeflection);
 
-  buildGraph(anglesOfDeflection)
+  buildGraph(anglesOfDeflection); 
+
+  animate(k, g, l, a, w, t);
+  
+  k = parseFloat(k);
+  l = parseFloat(l);
+  a = parseFloat(a);
+  w = parseFloat(w);
+  t = parseFloat(t);
 }
 
 var myChart
@@ -111,8 +113,6 @@ function downloadGraph() {
   });
 }
 
-// script.js
-
 function addToTable(k, g, l, a, w, t) {
   console.log("func work")
   // Get the table body
@@ -150,4 +150,74 @@ function addRow(tableBody, parameter, value) {
 
   var cellValue = newRow.insertCell(1);
   cellValue.textContent = value;
+}
+
+const canvas = document.getElementById("pendulumCanvas");
+const ctx = canvas.getContext("2d");
+
+const pendulum = {
+  originX: canvas.width / 2,
+  originY: canvas.height / 2,
+  rodLength: 100,
+  counter: 1,
+
+  update: function(k, g, l, a, w, t) {
+      const dt = 0.01;
+      const numSteps = 1000;
+
+      let f = 0.01;
+      let dfdt = 0;
+
+      var i = this.counter
+
+      if (i > 998) {
+          this.counter = 1;
+      }
+
+      const d2fdt2 = -2 * k * dfdt - (g / l - a * w * w * Math.cos(w * i * dt) / l) * Math.sin(f);
+      dfdt += d2fdt2 * dt;
+      f += dfdt * dt;
+      this.angle = f;
+      //console.log(this.angle)
+
+      // Обновление угловой скорости на основе гравитации
+      //this.angleVelocity += this.gravity * Math.sin(this.angle);
+
+      // Фактор затухания для постепенного уменьшения угловой скорости (для реалистичного движения)
+      //this.angleVelocity *= 0.99;
+
+      // Обновление угла на основе угловой скорости
+      //this.angle += this.angleVelocity;
+      this.counter += 1;
+  },
+  
+  draw: function() {
+      // Очистка холста
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Вычисление положения маятника
+      const bobX = this.originX + this.rodLength * Math.sin(this.angle);
+      const bobY = (this.originY + this.rodLength * (-Math.cos(this.angle)));
+      //console.log(`Value counter ${this.counter}`);
+      //console.log(`Value angle ${this.angle}`);
+      //console.log(`Value bobX: ${bobX}`);
+      //console.log(`Value bobY: ${bobY}`);
+
+      // Рисование стержня маятника
+      ctx.beginPath();
+      ctx.moveTo(this.originX, this.originY);
+      ctx.lineTo(bobX, bobY);
+      ctx.stroke();
+
+      // Рисование груза маятника
+      ctx.beginPath();
+      ctx.arc(bobX, bobY, 10, 0, 2 * Math.PI);
+      ctx.fill();
+  }
+};
+
+function animate(k, g, l, a, w, t) {
+  pendulum.update(k, g, l, a, w, t);
+  pendulum.draw();
+  requestAnimationFrame(animate);
 }
